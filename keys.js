2c682ce7413870221c478714763b3bb8b0da41f5
GreenCord <steve@nevizen.com>
2018-01-07 08:29:18 -0500

    Twitter and Spotify working

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
 
-// xbcgc18@gmail.com / @hidden_ocean_18
-
-var twitterKeys = {
-  consumer_key: 'gqyvvO2EIylNG1NezAGkM4iif',
-  consumer_secret: 'cLJWVIBBVq6ILTYxSwAUDDTnsXMZ7rK6KX4qWYYCRS09B7suJQ',
-  access_token_key: '948337160553402370-5ZYiFVEi9YBmUyNiVhPYihOHRAFkkxb',
-  access_token_secret: 'vHAduG7sTkKIdm88pa9bFoTeaV7Q0lBGUxaRju1zCD0MB',
-}
-
-module.exports = twitterKeys;
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,7 +1,24 @@
-var twitterKeys = require ('./keys.js');
+require("dotenv").config();
+var keys = require ('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
 
 
+var spotify = new Spotify(keys.spotify);
+var twitter = new Twitter(keys.twitter);
 
+var txt = {
+	loading: {
+		tweets: ',.-~*` --==[[[ LOADING TWEETS ]]]==-- `*~-.,',
+		spotify: ',.-~*` --==[[[ SEARCHING SPOTIFY ]]]==-- `*~-.,'
+	},
+	loaded: {
+		tweets: '`*~-., --==[[[ TWEETS LOADED  ]]]==-- ,.-~*`',
+		spotify: '`*~-., --==[[[ SPOTIFY SEARCHED  ]]]==-- ,.-~*`'
+	},
+	separator: '~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~'
+};
 
 var liri = {
 	cmd: process.argv[2],
@@ -13,7 +30,6 @@ var liri = {
 			else
 			{ this.input += process.argv[i]; }
 		}
-		console.log(this.input);
 		this.action();
 	},
 	action: function() {
@@ -39,8 +55,47 @@ var liri = {
 				console.log('Default case.');
 		} // end switch
 	}, // end action
-	tweets: function(arg){ console.log('Run "my-tweets"...'); },
-	spotify: function(arg){ console.log('Run "spotify-this-song"...', arg); },
+	tweets: function(){ 
+		console.log(txt.loading.tweets); 
+		var params = {screen_name: 'hidden_ocean_18'};
+		twitter.get('statuses/user_timeline', params, function(error, tweets, response){
+			if (!error) {
+				console.log(txt.loaded.tweets);
+				console.log(txt.separator);
+				for (var i = 0; i < tweets.length; i++) {
+					console.log(tweets[i].created_at + '\n' + tweets[i].text);
+					console.log(txt.separator);
+				}
+			}
+		});
+
+	},
+	spotify: function(arg){ 
+		console.log(txt.loading.spotify);
+		if ((!arg) || (arg == undefined)) {
+			arg = 'The Sign';
+		}
+		spotify.search({ type: 'track', query: arg, limit: 8 }, function(err, data) {
+		  if (err) {
+		    return console.log('Error occurred: ' + err);
+		  } else {
+		  	console.log(txt.loaded.spotify + '\n' + txt.separator);
+		  	var spotted = data.tracks.items;
+		  	for (var i = 0; i < spotted.length; i++) {
+		  		// console.log(spotted[i]);
+			  	console.log('Album:       ' + spotted[i].album.name);
+			  	console.log('Song Name:   ' + spotted[i].name);
+			  	for (var j = 0; j < spotted[i].album.artists.length; j++) {
+			  		console.log('Artist:      ' + spotted[i].album.artists[j].name);
+			  	}
+			  	console.log('Preview URL: ' + spotted[i].preview_url);
+			  	console.log(txt.separator);
+		  	}
+		  	
+		  	
+		  }
+		});
+	},
 	movie: function(arg){ console.log('Run "movie-this"...', arg); },
 	rando: function(arg){ console.log('Run "do-what-it-says"...'); }
 };
