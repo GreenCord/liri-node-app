b5db56a051000d98513c19487332d8c88cc229dd
GreenCord <steve@nevizen.com>
2018-01-07 10:21:08 -0500

    All working, formatting

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var fs = require('fs');
 var keys = require ('./keys.js');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
@@ -10,15 +11,28 @@ var spotify = new Spotify(keys.spotify);
 var twitter = new Twitter(keys.twitter);
 
 var txt = {
+	color: {
+		blue: '\x1b[1m\x1b[34m',
+		cyan: '\x1b[1m\x1b[36m',
+		cyandim: '\x1b[2m\x1b[36m',
+		green: '\x1b[1m\x1b[32m',
+		red: '\x1b[1m\x1b[31m',
+		reset: '\x1b[0m',
+		yellow: '\x1b[1m\x1b[33m',
+		white: '\x1b[1m\x1b[37m',
+		whitedim: '\x1b[2m\x1b[37m'
+	},
 	loading: {
-		tweets: ',.-~*` --==[[[ LOADING TWEETS ]]]==-- `*~-.,',
-		spotify: ',.-~*` --==[[[ SEARCHING SPOTIFY ]]]==-- `*~-.,',
-		movie: ',.-~*` --==[[[ LOADING OMDB ]]]==-- `*~-.,'
+		tweets: ',.-~*` --==[[[\x1b[1m\x1b[33m LOADING TWEETS \x1b[0m]]]==-- `*~-.,',
+		spotify: ',.-~*` --==[[[\x1b[1m\x1b[33m SEARCHING SPOTIFY \x1b[0m]]]==-- `*~-.,',
+		movie: ',.-~*` --==[[[\x1b[1m\x1b[33m SEARCHING OMDB \x1b[0m]]]==-- `*~-.,',
+		do: ',.-~*` --==[[[\x1b[1m\x1b[33m DOING WHAT IT SAYS \x1b[0m]]]==-- `*~-.,'
 	},
 	loaded: {
-		tweets: '`*~-., --==[[[ TWEETS LOADED  ]]]==-- ,.-~*`',
-		spotify: '`*~-., --==[[[ SPOTIFY SEARCHED  ]]]==-- ,.-~*`',
-		movie: ',.-~*` --==[[[ OMDB LOADED  ]]]==-- `*~-.,'
+		tweets: '`*~-., --==[[[\x1b[1m\x1b[32m TWEETS LOADED  \x1b[0m]]]==-- ,.-~*`',
+		spotify: '`*~-., --==[[[\x1b[1m\x1b[32m SPOTIFY SEARCHED  \x1b[0m]]]==-- ,.-~*`',
+		movie: '`*~-., --==[[[\x1b[1m\x1b[32m OMDB SEARCHED  \x1b[0m]]]==-- ,.-~*`',
+		do: '`*~-., --==[[[\x1b[1m\x1b[32m  DOING THE THING   \x1b[0m]]]==-- ,.-~*`'
 	},
 	separator: '~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~',
 	separatorplain: '-------------------------------------------------------------'
@@ -54,7 +68,15 @@ var liri = {
 			break;
 
 			default:
-				console.log('Default case.');
+				console.log(txt.separator);
+				console.log(txt.color.red + 'Sorry, I need to know what you want to do. I know the following commands:' + txt.color.reset);
+				console.log(txt.separatorplain);
+				console.log(txt.color.green + 'my-tweets' + txt.color.reset + ' - Display tweets.');
+				console.log(txt.color.green + 'spotify-this-song' + txt.color.cyan + ' song name' + txt.color.reset + ' - Check spotify for a song.');
+				console.log(txt.color.green + 'movie-this' + txt.color.cyan + ' movie name' + txt.color.reset + ' - Check OMDB for info about a movie.');
+				console.log(txt.color.green + 'do-what-it-says' + txt.color.reset + ' - If you feel lucky.');
+				console.log(txt.separatorplain);
+				console.log(txt.separator);
 		} // end switch
 	}, // end action
 	tweets: function(){ 
@@ -64,10 +86,12 @@ var liri = {
 			if (!error) {
 				console.log(txt.loaded.tweets);
 				console.log(txt.separator);
+				console.log(txt.separatorplain);
 				for (var i = 0; i < tweets.length; i++) {
-					console.log(tweets[i].created_at + '\n' + tweets[i].text);
-					console.log(txt.separator);
+					console.log(txt.color.cyan + tweets[i].created_at + txt.color.white + '\n' + tweets[i].text + txt.color.reset);
+					console.log(txt.separatorplain);
 				}
+				console.log(txt.separator);
 			}
 		});
 
@@ -89,12 +113,12 @@ var liri = {
 		  	} else {
 			  	for (var i = 0; i < spotted.length; i++) {
 			  		// console.log(spotted[i]);
-				  	console.log('Album:       ' + spotted[i].album.name);
-				  	console.log('Song Name:   ' + spotted[i].name);
+				  	console.log(txt.color.cyan + 'Album:       ' + txt.color.white + spotted[i].album.name);
+				  	console.log(txt.color.cyan + 'Song Name:   ' + txt.color.white + spotted[i].name);
 				  	for (var j = 0; j < spotted[i].album.artists.length; j++) {
-				  		console.log('Artist:      ' + spotted[i].album.artists[j].name);
+				  		console.log(txt.color.cyan + 'Artist:      ' + txt.color.white + spotted[i].album.artists[j].name);
 				  	}
-				  	console.log('Preview URL: ' + spotted[i].preview_url);
+				  	console.log(txt.color.cyan + 'Preview URL: ' + txt.color.white + spotted[i].preview_url + txt.color.reset);
 				  	console.log(txt.separator);
 			  	}
 			  }
@@ -120,9 +144,9 @@ var liri = {
 			body = JSON.parse(body);
 			if ((error) || body.Response == 'False') {
 				if (error) {
-					console.log('Error occurred: ' + error);
+					console.log(txt.color.red + 'Error occurred: ' + error + txt.color.reset);
 				} else {
-					console.log('Error occurred: ' + body.Error);
+					console.log(txt.color.red + 'Error occurred: ' + body.Error + txt.color.reset);
 				}
 			} else {
 				console.log(txt.loaded.movie);
@@ -140,12 +164,16 @@ var liri = {
 				}
 				console.log(txt.separator);
 				// console.log('body:', body);
-				console.log(body.Title + ' (' + body.Year + ', ' + body.Country + ')' + ' | IMDB Rating ['+ ratingsImdb +']' + ' | Rotten Tomatoes ['+ ratingsRotten +']');
-				console.log(body.Language);
+				console.log(txt.color.cyan + body.Title + 
+					' (' + txt.color.reset + txt.color.cyandim + body.Year + ', ' + body.Country + txt.color.cyan + ')' + 
+					txt.color.blue + ' | ' + txt.color.cyan + 'IMDB Rating ['+ txt.color.reset + txt.color.cyandim + ratingsImdb + txt.color.cyan + ']' + 
+					txt.color.blue + ' | ' + txt.color.cyan + 'Rotten Tomatoes ['+ txt.color.reset + txt.color.cyandim + ratingsRotten + txt.color.cyan + ']' +
+					txt.color.reset);
+				console.log(txt.color.white + body.Language + txt.color.reset);
 				console.log(txt.separatorplain);
-				console.log(body.Actors);
+				console.log(txt.color.white + body.Actors + txt.color.reset);
 				console.log(txt.separatorplain);
-				console.log(body.Plot);
+				console.log(txt.color.white + body.Plot + txt.color.reset);
 				console.log(txt.separator);
 			}
 		  
@@ -153,7 +181,26 @@ var liri = {
 		
 
 	},
-	rando: function(arg){ console.log('Run "do-what-it-says"...'); }
+	rando: function(arg){ 
+		console.log(txt.loading.do); 
+		var doWhat;
+		var withThis;
+		fs.readFile('./random.txt', 'utf8', function(err,data){
+			if (err) throw err;
+			console.log(txt.loaded.do); 
+			var arr = data.split(',');
+			doWhat = arr[0];
+			withThis = arr[1].replace(/"/g,'');
+			console.log('\n' + txt.color.white + 'Do What: ' + txt.color.reset + txt.color.cyan + doWhat);
+			console.log(txt.color.white + 'With This: ' + txt.color.reset + txt.color.cyan + withThis + txt.color.reset + '\n');
+			liri.cmd = doWhat;
+			liri.input = withThis;
+			var doNewLiri = liri.action;
+			var newRun = doNewLiri.bind(liri);
+			newRun();
+		});
+
+	}
 };
 var doLiri = liri.init;
 var run = doLiri.bind(liri);
